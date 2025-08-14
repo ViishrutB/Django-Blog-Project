fd2518adcb2f6b52a7167ad89e2133c0db3e4359
viishrut <viishrut@gmail.com>
2025-08-14 09:06:57 -0400

    Adding Pagination to the Blog App

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,33 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from .models import Post
-from django.http import Http404
 
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
+    # Pagination with 3 Posts per Page
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page number is out of range, get the last page of the results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # If page number is not an integer, get the first page
+        posts = paginator.page(1)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post, publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
