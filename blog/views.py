1230e9059168c3ac073abb318b1880c42398c272
viishrut <viishrut@gmail.com>
2025-09-11 21:10:49 -0400

    Building a search view

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import SearchVector
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
 
@@ -132,4 +133,28 @@ def post_comment(request, post_id):
         request,
         'blog/post/comment.html',
         {'post': post, 'form': form, 'comment': comment}
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                ).filter(search=query)
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
     )
